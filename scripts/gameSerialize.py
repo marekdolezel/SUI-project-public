25dd1bcd1b4ee9ee2635521332bca2cfd0660544
Marek Dolezel <xdolez67@stud.fit.vutbr.cz>
2021-12-31 11:26:17 +0100

    process_pickle_to_np_array.py: speed up serialization

--- scripts/gameSerialize.py
+++ scripts/gameSerialize.py
@@ -1,7 +1,11 @@
+"""
+@author: Marek Dolezel
+"""
 from matplotlib import pyplot as plt
 import numpy as np
 from datetime import datetime
 import os
+
 def helper_plot_matrix_M(M):
     range = np.arange(1, 35, 1)
     plt.imshow(M, interpolation='nearest')
@@ -11,13 +15,40 @@ def serialize_game_state(board, players, globalPlayersDict):
     M,o,d = _serialize_game_state(board, players, globalPlayersDict)
     return _game_state_to_vector(M,o,d)
 
+def serialize_game_state_fast(board, players, globalPlayersDict):
+    bSize = 34
+    gameStateVectorSize = (bSize ** 2 - bSize) / 2 + bSize + 2*bSize
+    gameStateVector = np.zeros((int(gameStateVectorSize)))
+
+    ownershipIndex = int((bSize ** 2 - bSize) / 2 + bSize)
+    diceIndex = int((bSize ** 2 - bSize) / 2 + bSize + bSize)
+
+    for i in range(1, bSize + 1):
+        area = board.get_area_by_name(i) # Areas are indexed from 1 to 34
+        owner_name = area.owner_name
+        areaIndex = area.name - 1
+
+        new_owner_name = globalPlayersDict[players[owner_name]]
+        gameStateVector[ownershipIndex] = new_owner_name
+        gameStateVector[diceIndex] = area.dice
+
+        ownershipIndex = ownershipIndex + 1
+        diceIndex = diceIndex + 1
+
+        neighbours = area.get_adjacent_areas()
+        for neighbour_area in neighbours:
+            neighbourIndex = neighbour_area.name - 1
+            if areaIndex <= neighbourIndex: # uppertriangle
+                matrixIndex = areaIndex * bSize + neighbourIndex - areaIndex*(1+areaIndex)/2
+                gameStateVector[int(matrixIndex)] = 1
+    return gameStateVector
+
+
 def _serialize_game_state(board, players, globalPlayersDict):
     bSize = 34
     adjacencyMatrix = np.zeros((bSize, bSize))
     ownerShipVector = np.zeros((bSize))
     diceVector = np.zeros((bSize))
-
-    # board.get_board()
 
     for i in range(1, bSize + 1):
         area = board.get_area_by_name(i) # Areas are indexed from 1 to 34
--- scripts/process_pickle_to_np_array.py
+++ scripts/process_pickle_to_np_array.py
@@ -3,7 +3,7 @@ import pickle
 from multiprocessing import Process, cpu_count
 import os,sys
 import numpy as np
-from scripts.gameSerialize import serialize_game_state, save_game_state_vector_to_file
+from scripts.gameSerialize import serialize_game_state, save_game_state_vector_to_file, serialize_game_state_fast
 # Pickle file format:
 # containst tuple of objects (players, arrOfBoards),
 #   where 'players' object is a dictionary mapping player_name to player_nickname
@@ -21,6 +21,7 @@ def compute_globalPlayersDict(players, globalPlayersDict, maxIndex):
                 maxIndex = maxIndex + 1
     return globalPlayersDict, maxIndex
 
+
 def compute_winner(lastBoard):
     winner = -1
     for i in range(1, 34 + 1):
@@ -32,6 +33,7 @@ def compute_winner(lastBoard):
         else:
             return -1
     return winner
+
 
 def process_pickles(files_to_process, rootdir):
     globalPlayersDict = {}
@@ -53,13 +55,18 @@ def process_pickles(files_to_process, rootdir):
 
             # gameStateVectorsArray = serialize_game_state(arrOfBoards[0], players, globalPlayersDict)
             # gameStateVectorsArray = np.append(gameStateVectorsArray, winner) # add winner to the vector
-            for board in arrOfBoards[1:]:
-                gameStateVector = serialize_game_state(board, players, globalPlayersDict)
+            for board in arrOfBoards:
+                gameStateVector = serialize_game_state_fast(board, players, globalPlayersDict)
+                # a = serialize_game_state(board, players, globalPlayersDict)
+                # if not np.array_equal(a, gameStateVector):
+                #     exit(2)
                 gameStateVector = np.append(gameStateVector, winner)  # add winner to the vector
                 gameStateVectorsArray = np.vstack((gameStateVectorsArray, gameStateVector))
             gameStateVectorsArray = np.delete(gameStateVectorsArray, (0), axis=0)
     save_game_state_vector_to_file(gameStateVectorsArray)
 
+
+# 1 434 390
 def split(a, n):
     k, m = divmod(len(a), n)
     return (a[i*k+min(i, m):(i+1)*k+min(i+1, m)] for i in range(n))
@@ -68,9 +75,15 @@ def split(a, n):
 if __name__ == '__main__':
     # Post process files in parallel
     ListOfFilenames = os.listdir(sys.argv[1])
+
+    # One if processing all files, other number if you want to process less files
+    Parts = split(ListOfFilenames, int(sys.argv[3]))
+    ListOfFilenames = list(Parts)[1]
+
     ListOfProcesses = []
     Processors = cpu_count()  # n of processors you want to use
     print("detected ",cpu_count(), "processors")
+    print("will be processing:", len(ListOfFilenames)," Files...")
     # Divide the list of files in 'n of processors' Parts
     Parts = split(ListOfFilenames, Processors)
     if (sys.argv[2] == "single"): # if single is specified process files in sequential order
