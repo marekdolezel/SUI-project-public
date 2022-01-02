f2e35a80c5c77d61a7fbc0f0df4212681b16423e
Marek Dolezel <xdolez67@stud.fit.vutbr.cz>
2022-01-02 21:49:36 +0100

    script for submission

---
+++ scripts/submit_it.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SUBMIT_DIR=submission
+LEADER=xdolez67
+SUPP_DIR=$SUBMIT_DIR/supp-$LEADER
+
+mkdir $SUBMIT_DIR
+mkdir -p $SUPP_DIR
+cp -r  dicewars/ai/kb/xdolez67 $SUBMIT_DIR
+
+cp ./scripts/process_pickle_to_np_array.py $SUPP_DIR
+cp ./scripts/gameSerialize.py $SUPP_DIR
+cp ./scripts/network.py $SUPP_DIR
+cp ./scripts/extract_subset_fromH5.py $SUPP_DIR
+
+zip -r $LEADER $SUBMIT_DIR/*
