264e93f2aee9e7f3a28c9f03eb89838d5ac96da0
Marek Dolezel <xdolez67@stud.fit.vutbr.cz>
2022-01-02 16:27:18 +0100

    extract_subset_fromH5.py: name of the output file is specified by argv[2]

--- scripts/extract_subset_fromH5.py
+++ scripts/extract_subset_fromH5.py
@@ -2,9 +2,9 @@
 import os,sys
 import numpy as np
 import h5py
-
+# python3 ./scripts/extract_subset_fromH5.py largeInputFile, outputFile
 if __name__ == '__main__':
-    h5dataset = h5py.File('UniformDataset210K.h5', 'a')
+    h5dataset = h5py.File(sys.argv[2], 'a')
 
     winnerArrays = {}
 
@@ -14,10 +14,11 @@ if __name__ == '__main__':
 
     np.random.shuffle(data)
 
+    # select 50,000 rows for each player
     for i in range(1,7):
         winnerArrays[str(i)] = data[np.where(data[:, 663] == i)]
         np.random.shuffle(winnerArrays[str(i)])
-        winnerArrays[str(i)] = winnerArrays[str(i)][0:35000,:]
+        winnerArrays[str(i)] = winnerArrays[str(i)][0:50000,:]
 
     # merge np array into one:
     npArray = winnerArrays['1']
