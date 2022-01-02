5a9ecbc55331c63b810d923201f8aa2d3fc53ce8
tulakr <radsetoulal.v@gmail.com>
2022-01-02 03:03:18 +0100

    Almost done, contains todo list.
    Added functions for model training, testing, saving/loading. Adjusted model architecture.

--- scripts/network.py
+++ scripts/network.py
@@ -5,31 +5,33 @@ Created on Thu Dec 30 17:40:14 2021
 
 @author: Vlastimil Radsetoulal
 """
-from matplotlib import pyplot as plt
+
 import numpy as np
 import pickle
-from datetime import datetime
 
 import torch
 from torch import nn
 from torch.utils.data import DataLoader
 from torchvision import datasets
+from torchvision import transforms
 from torchvision.transforms import ToTensor, Lambda, Compose
 import process_pickle_to_np_array as pitonp
+import h5py
 
-#Neural network script, will initialize NN, train it, save it, take data from npy file
+#Neural network script, will initialize NN, train it, save it, take data from h5 file
 # or the data can be rather passed by some other already complete script
 # then -> model will be called for evaluation of each state of MaxN 
 
-#Warning -> this needs gameStates file to be in scripts. After model is trained this script won't need to be launched TODO
-
 #Questions:
-    # Do we have data of each state or only of initiate states ?
-    # Do players always have the same numbers ? If not we need to asure it
-    # Do we know in the adjucency matrice always determine which player we are ? 
-    # is it adjucency of our player/AI ? If yes - that's great !
+#TODO - ValueError: not enough values to unpack (expected 2, got 1)
+#error line 63 - check dataloader / data provided
+#TODO epochs iteration (one simple for)
+#TODO check data with tainted
 
-
+    
+device = "cuda" if torch.cuda.is_available() else "cpu"
+loss_fn = nn.CrossEntropyLoss()
+transform = transforms.Compose([transforms.ToTensor()])
 
 # TODO supervised learning NN with outputs - WIN/LOSS inputs -> vector of numbers from gameSerialize
 class NetworkSui(nn.Module):
@@ -38,13 +40,14 @@ class NetworkSui(nn.Module):
         super(NetworkSui, self).__init__()
         self.flatten = nn.Flatten()
         self.linear_relu_stack = nn.Sequential(
-            nn.Linear(663, 330),
+            nn.Linear(663, 512),
             nn.ReLU(),
-            nn.Linear(330, 165),
+            nn.Linear(256, 128),
             nn.ReLU(),
-            nn.Linear(165, 10),
+            nn.Linear(128, 64),
             nn.ReLU(),
-            nn.Linear(10, 1)
+            nn.Linear(64, 4),
+            nn.Softmax(dim=1)
         )
         
     def forward(self, x):
@@ -52,30 +55,67 @@ class NetworkSui(nn.Module):
         logits = self.linear_relu_stack(x)
         return logits
 
+
+    def train_model(model, dataloader):
+        model.train()
+        size = len(dataloader.dataset)
+        
+        for batch, (X, y) in enumerate(dataloader, 0):
+            X, y = X.to(device), y.to(device)
+            
+            pred = model(X)
+            loss = nn.CrossEntropyLoss(pred, y)
+            
+            nn.optimizer.zero_grad()
+            loss.backward()
+            nn.optimizer.step()
+            
+        if batch % 100 == 0:
+            loss, current = loss.item(), batch * len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+     
+        
+    def test(dataloader, model, loss_fn):
+        size = len(dataloader.dataset)
+        num_batches = len(dataloader)
+        model.eval()
+        test_loss, correct = 0, 0
+        with torch.no_grad():
+            for X, y in dataloader:
+                X, y = X.to(device), y.to(device)
+                pred = model(X)
+                test_loss += loss_fn(pred, y).item()
+                correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+        test_loss /= num_batches
+        correct /= size
+        print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+        
 #extract and process gameStates files (to inputs and outputs)
     def extract_states():
         
-        data_array = np.load("gameStates/1230_2021_195436.npy")      #change this - only processess one file TODO
-        
-        outputs = np.zeros((data_array.shape[0],1))
-        inputs = np.zeros((data_array.shape[0],663))
-        
-        index = 0
-        for row in data_array:
-            outputs[index] = row[-1] if row[-1]==1 else 0       #1 if player #1 wins otherwise 0 (lost) which number is our AI ? TODO
-            inputs[index] = data_array[index][:-1]
-            index+=1
-            
+        h5f = h5py.File('processed667files.h5','r')
 
-        data = (inputs, outputs)
+        data_array = h5f['data'][:]
+        h5f.close()
+
+        data = transform(data_array)
+        data = DataLoader(data, batch_size=64)
+
         return data
     
-    
-    #fit model with inputs outputs, iterate epochs TODO
-    def train_model():
-        return 0
+    def save_model(model):
+        torch.save(model.state_dict(), "model.pth")             #change dir TODO
+        print("Modal has been saved as model.pth")
+
+
+    def load_model():
+        model = NetworkSui()
+        model.load_state_dict(torch.load("model.pth"))          #check dir ...
         
 #for testing, might be removed entirely later        
 if __name__ == "__main__":
-    data_tuple = NetworkSui.extract_states()
-    #print(data_tuple)                                           check how data tuple looks if you want
+    data = NetworkSui.extract_states()
+    model = NetworkSui().to(device)
+
+    NetworkSui.train_model(model, data)
+    NetworkSui.save_model(model)
